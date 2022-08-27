ba6c57e53959d66e921b9ba0b491a754e878c2e4
PERSON1203 <41105113+PERSON1203@users.noreply.github.com>
2022-08-28 03:23:27 +0900

    dnd-kit ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
+import {DndContext} from '@dnd-kit/core';
+
+import {Draggable} from './Draggable';
+import {Droppable} from './Droppable';
+
 import './App.css';
 
 function App() {
+  const containers = ['A', 'B', 'C'];
+  const [parent, setParent] = useState(null);
+  const draggableMarkup = (
+    <Draggable id="draggable">Drag me</Draggable>
+  );
+
+
+  function handleDragEnd(event: any) {
+    const {over} = event;
+
+    // If the item is dropped over a container, set it as the parent
+    // otherwise reset the parent to `null`
+    setParent(over ? over.id : null);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +39,20 @@ function App() {
           Learn React
         </a>
       </header>
+      <DndContext onDragEnd={handleDragEnd}>
+      {parent === null ? draggableMarkup : null}
+
+      {containers.map((id) => (
+        // We updated the Droppable component so it would accept an `id`
+        // prop and pass it to `useDroppable`
+        <Droppable key={id} id={id}>
+          {parent === id ? draggableMarkup : 'Drop here'}
+        </Droppable>
+      ))}
+    </DndContext>
+
     </div>
+    
   );
 }
 
